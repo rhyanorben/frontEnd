f08acf77a0af005f8e21fd77dd885bf46e8eaf5b
Rhyan Willemann Orben <rhyanorben@gmail.com>
2024-03-25 20:18:47 -0300

    aula 06

---
+++ 2024-03-25 - Aula 06/script.js
@@ -0,0 +1,27 @@
+console.log(typeof 1)
+console.log(typeof 'teste')
+console.log(typeof 1.2)
+console.log(typeof [0,1,2,3,4])
+console.log(2 - 5)
+
+let a = 2
+const b = 3
+
+console.log(`teste ${a} e ${b}`)
+
+a = 5
+
+console.log(b + ' teste ' + a)
+console.log('3' == a)
+
+switch (a) {
+    case 3:
+        console.log('resposta 1')
+        break;
+    case 2:
+        console.log('resposta 2')
+        break;
+    default:
+        console.log('default')
+        break;
+}
